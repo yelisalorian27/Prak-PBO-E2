01aad23fb89ab631e64fea398b0a180d49d96d48
yelisalorian27 <yelisalorian04@gmail.com>
2025-06-04 20:30:38 +0700

    Add files via upload

---
+++ Pertemuan 8/Koleksi 3.4/Koleksi 3.4/src/BangunDatar/BangunDatarGenerikTest.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BangunDatar;
+
+/**
+ *
+ * @author yelisalorian
+ */
+/**
+ * File : BangunDatarGenericTest.java
+ * Deskripsi : main class untuk generic bangun datar
+ */
+
+public class BangunDatarGenerikTest {
+    public static void main(String[] args) {
+        // Lingkaran
+        BangunDatarGenerik<Lingkaran> lingkaran = new BangunDatarGenerik<>();
+        lingkaran.set(new Lingkaran(7));
+        System.out.println("Lingkaran:");
+        System.out.println("Luas: " + lingkaran.hitungLuas());
+        System.out.println("Keliling: " + lingkaran.hitungKeliling());
+        System.out.println();
+
+        // Persegi
+        BangunDatarGenerik<Persegi> persegi = new BangunDatarGenerik<>();
+        persegi.set(new Persegi(4));
+        System.out.println("Persegi:");
+        System.out.println("Luas: " + persegi.hitungLuas());
+        System.out.println("Keliling: " + persegi.hitungKeliling());
+        System.out.println();
+
+        // Persegi Panjang
+        BangunDatarGenerik<PersegiPanjang> pp = new BangunDatarGenerik<>();
+        pp.set(new PersegiPanjang(5, 3));
+        System.out.println("Persegi Panjang:");
+        System.out.println("Luas: " + pp.hitungLuas());
+        System.out.println("Keliling: " + pp.hitungKeliling());
+        System.out.println();
+
+        // Segitiga
+        BangunDatarGenerik<Segitiga> segitiga = new BangunDatarGenerik<>();
+        segitiga.set(new Segitiga(6, 4, 5, 5)); // alas, tinggi, sisi1, sisi2
+        System.out.println("Segitiga:");
+        System.out.println("Luas: " + segitiga.hitungLuas());
+        System.out.println("Keliling: " + segitiga.hitungKeliling());
+        System.out.println();
+    }
+}
---
+++ Pertemuan 8/Koleksi 3.4/Koleksi 3.4/src/koleksi/pkg3/pkg4/Koleksi34.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package koleksi.pkg3.pkg4;
+
+/**
+ *
+ * @author yelisalorian
+ */
+public class Koleksi34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
